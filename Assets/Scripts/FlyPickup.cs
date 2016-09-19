c644822e29a5c11e678fb298b71b4d14bfc85315
shinmiy <shinmiyaccs@gmail.com>
2016-09-19 19:51:53 +0900

    Create the Pickup Spawner

--- Assets/Scripts/FlyPickup.cs
+++ Assets/Scripts/FlyPickup.cs
@@ -3,11 +3,16 @@
 public class FlyPickup : MonoBehaviour
 {
 
+    [SerializeField]
+    private GameObject pickupPrefab;
+
     void OnTriggerEnter(Collider other)
     {
 		// If the Collider other is tagged with "Player"...
         if (other.CompareTag("Player"))
         {
+            // ... add the pickup particles...
+            Instantiate(pickupPrefab, transform.position, Quaternion.identity);
             Destroy(gameObject);
         }
     }
---
+++ Assets/Scripts/PickupParticlesDestruction.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickupParticlesDestruction : MonoBehaviour {
+
+    // Use this for initialization
+    void Start()
+    {
+        // Destroy the pickup particles after 5 seconds;
+        Destroy(gameObject, 5.0f);
+    }
+}
