ee5eda0b5219825fdab3d8ca02c8cac25d0f5f1b
shinmiy <shinmiyaccs@gmail.com>
2016-09-19 16:11:39 +0900

    C# Variables and Classes

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour {
 
+	private Animator playerAnimator;
+	private float moveHorizontal;
+	private float moveVertical;
+	private Vector3 movement;
+
 	// Use this for initialization
 	void Start () {
 		
