d63752ee6b8710f62f982afd3884969233a0c9f6
shinmiy <shinmiyaccs@gmail.com>
2016-09-19 18:38:31 +0900

    Make a Follow Camera

---
+++ Assets/Scripts/FollowCamera.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class FollowCamera : MonoBehaviour
+{
+
+    [SerializeField]
+    private Transform player;
+    [SerializeField]
+    private Vector3 offset;
+    private float cameraFollowSpeed = 5f;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 newPosition = player.position + offset;
+
+        transform.position = Vector3.Lerp(transform.position, newPosition, cameraFollowSpeed + Time.deltaTime);
+    }
+}
